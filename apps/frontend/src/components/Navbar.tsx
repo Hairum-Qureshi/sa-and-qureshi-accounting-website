e780f16a6bc00afad21d59eb773776e316fd4559
Hairum Qureshi <hqureshi@udel.edu>
2026-07-01 17:05:39 -0400

    created basic navbar with tax company name

---
+++ apps/frontend/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <div className="p-4 flex items-center w-full h-20">
+      <div className="flex flex-col items-start text-xl font-semibold">
+        <h3>SA & Qureshi</h3>
+        <h3>Tax Accounting Services</h3>
+      </div>
+    </div>
+  );
+}
