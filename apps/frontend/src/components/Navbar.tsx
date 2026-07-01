af4db79de68699d0edc3c004651654a5928bd78d
Hairum Qureshi <hqureshi@udel.edu>
2026-07-01 18:39:36 -0400

    added hashlinks to the navbar

--- apps/frontend/src/components/Navbar.tsx
+++ apps/frontend/src/components/Navbar.tsx
@@ -1,9 +1,27 @@
+import { HashLink } from "react-router-hash-link";
+
 export default function Navbar() {
   return (
     <div className="p-4 flex items-center w-full h-20">
-      <div className="flex flex-col items-start text-xl font-semibold">
+      <div className="flex flex-col items-start text-xl font-semibold text-blue-500">
         <h3>SA & Qureshi</h3>
         <h3>Tax Accounting Services</h3>
+      </div>
+      <div className="flex flex-row items-center ml-auto space-x-4 text-base text-blue-600 font-semibold">
+        <HashLink
+          smooth
+          to="#services"
+          className="hover:underline hover:underline-offset-6"
+        >
+          <span className="ml-4">SERVICES</span>
+        </HashLink>
+        <HashLink
+          smooth
+          to="#connect"
+          className="hover:underline hover:underline-offset-6"
+        >
+          <span className="ml-4">CONNECT</span>
+        </HashLink>
       </div>
     </div>
   );
