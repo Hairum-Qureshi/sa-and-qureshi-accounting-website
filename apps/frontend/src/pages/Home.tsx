2c3d50413dd473a58031a17c1b21948cbc5e0134
Hairum Qureshi <hqureshi@udel.edu>
2026-07-01 18:23:17 -0400

    added Footer component

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import Connect from "../components/Connect";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Services from "../components/Services";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Services />
+      <Connect />
+      <Footer />
     </div>
   );
 }
