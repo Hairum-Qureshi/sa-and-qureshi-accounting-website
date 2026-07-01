6e9d29c9121613ae9b7ade7c0a00614a1200c395
Hairum Qureshi <hqureshi@udel.edu>
2026-07-01 18:22:24 -0400

    removed react-query

--- apps/frontend/src/components/main.tsx
+++ apps/frontend/src/components/main.tsx
@@ -1,17 +1,10 @@
 import App from "./App.tsx";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "../css/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-			<ReactQueryDevtools />
-		</QueryClientProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
