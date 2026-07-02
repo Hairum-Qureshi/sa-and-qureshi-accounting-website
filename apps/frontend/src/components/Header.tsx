b30101510e51536717ecd3a32d963250e6be1b50
Hairum Qureshi <hqureshi@udel.edu>
2026-07-02 00:00:25 -0400

    changed color theme to green

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -2,21 +2,24 @@ import { FaCheckCircle } from "react-icons/fa";
 
 export default function Header() {
   return (
-    <div className="bg-[#f0f6fd] p-15 lg:flex">
+    // <div className="bg-[#f0f6fd] p-15 lg:flex">
+    <div className="bg-green-100 p-15 lg:flex">
       <div className="w-full lg:w-1/2 lg:mx-8">
-        <h3 className="text-lg font-semibold text-blue-500">
+        <h3 className="text-lg font-semibold text-green-500">
           PROFESSIONAL. RELIABLE. TRUSTED.
         </h3>
         <h1 className="text-6xl font-bold leading-14">
           Accounting & Tax <br /> Solutions You Can Trust
         </h1>
         <p className="text-lg mt-4">
-          SA Accounting & Qureshi's Tax Accounting Services are here to help you
-          with personalized and reliable services for you, your family, and your
-          friends.
+          <b className="text-2xl">
+            SA Accounting & Qureshi's Tax Accounting Services
+          </b>{" "}
+          are here to help you with personalized and reliable services for you,
+          your family, and your friends.
         </p>
         <button
-          className="bg-blue-500 text-white hover:cursor-pointer px-4 py-2 rounded mt-4 hover:bg-blue-600 transition-colors duration-300"
+          className="bg-green-500 text-white hover:cursor-pointer px-4 py-2 rounded mt-4 hover:bg-green-600 transition-colors duration-300"
           onClick={() => {
             const connectSection = document.getElementById("connect");
             if (connectSection) {
@@ -28,37 +31,37 @@ export default function Header() {
         </button>
       </div>
       <div className="flex-1 mx-8 text-lg lg:my-0 my-10">
-        <h3 className="font-semibold text-blue-500">
+        <h3 className="font-semibold text-green-500">
           WE'RE HERE TO HELP WITH...
         </h3>
         <div className="my-3 flex flex-col gap-3">
           <p className="flex">
             <span className="flex-row items-center mr-2">
-              <FaCheckCircle className="text-blue-500" />
+              <FaCheckCircle className="text-green-500" />
             </span>
-            Personalized Service
+            Personalized Services
           </p>
           <p className="flex">
             <span className="flex-row items-center mr-2">
-              <FaCheckCircle className="text-blue-500" />
+              <FaCheckCircle className="text-green-500" />
             </span>
             Accurate & Timely Filings
           </p>
           <p className="flex">
             <span className="flex-row items-center mr-2">
-              <FaCheckCircle className="text-blue-500" />
+              <FaCheckCircle className="text-green-500" />
             </span>
             Confidentiality & Security
           </p>
           <p className="flex">
             <span className="flex-row items-center mr-2">
-              <FaCheckCircle className="text-blue-500" />
+              <FaCheckCircle className="text-green-500" />
             </span>
             Affordable Rates
           </p>
           <p className="flex">
             <span className="flex-row items-center mr-2">
-              <FaCheckCircle className="text-blue-500" />
+              <FaCheckCircle className="text-green-500" />
             </span>
             Experienced Support
           </p>
