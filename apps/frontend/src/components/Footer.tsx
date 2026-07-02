72119a6824fad7f91fb014c41275a640a8384d49
Hairum Qureshi <hqureshi@udel.edu>
2026-07-02 00:47:07 -0400

    fixed issue with shrinking GitHub icon and also replaced <a> tags with <Link> tags

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -1,23 +1,23 @@
 import { FaGithub } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer className="bg-green-950 text-white">
       <div className="mx-auto flex h-16 items-center justify-between px-6">
-        <p className="text-sm">
+        <p className="lg:text-sm text-xs">
           &copy; {new Date().getFullYear()} SA Accounting & Qureshi's Tax
           Accounting Services. All rights reserved.
         </p>
-
-        <a
-          href="https://github.com/Hairum-Qureshi"
+        <Link
+          to="https://github.com/Hairum-Qureshi"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex items-center gap-2 text-xs text-white/50 transition hover:text-white"
+          className="flex items-center gap-2 whitespace-nowrap text-xs text-white/50 transition hover:text-white"
         >
           <span>Website by Hairum Qureshi</span>
-          <FaGithub className="w-4 h-4" />
-        </a>
+          <FaGithub className="h-5 w-5 shrink-0" />
+        </Link>
       </div>
     </footer>
   );
