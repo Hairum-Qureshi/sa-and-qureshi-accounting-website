10b1a92fff4dbdab5fa29eeafa7a3b07d66e4a44
Hairum Qureshi <hqureshi@udel.edu>
2026-07-05 19:27:14 -0400

    added navbar to Home component

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Connect from "../components/Connect";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Services from "../components/Services";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Header />
       <Services />
       <Connect />
