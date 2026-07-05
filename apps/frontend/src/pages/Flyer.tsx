28557e970207c7d983364e1f251ca479b064a2e0
Hairum Qureshi <hqureshi@udel.edu>
2026-07-05 18:56:50 -0400

    created flyer component

---
+++ apps/frontend/src/pages/Flyer.tsx
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+import Services from "../components/Services";
+
+export default function Flyer() {
+  return (
+    <div className="border-2 border-black rounded-md p-3 w-3/4 mx-auto mt-10">
+      <Header
+        customTitle={"Qureshi's Tax Accounting Services"}
+        showSAAccounting={false}
+        updatedDesc={
+          "is here to help you with personalized and reliable services for you, your business, family, and friends"
+        }
+        showContactButton={false}
+      />
+      <Services />
+    </div>
+  );
+}
