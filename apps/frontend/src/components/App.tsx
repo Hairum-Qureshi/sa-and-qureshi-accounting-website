ffaa4ba5ae38d4c7d812935b4a26f0394dbd1a66
Hairum Qureshi <hqureshi@udel.edu>
2026-07-05 20:03:30 -0400

    removed flyer endpoint

--- apps/frontend/src/components/App.tsx
+++ apps/frontend/src/components/App.tsx
@@ -2,14 +2,12 @@ import "../css/index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
-import Flyer from "../pages/Flyer";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/flyer" element={<Flyer />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
