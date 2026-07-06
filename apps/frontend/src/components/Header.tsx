6c4fdd7da3e728f68e951b83877b25f1fb35bea5
Hairum Qureshi <hqureshi@udel.edu>
2026-07-05 20:07:04 -0400

    reverted changes

--- apps/frontend/src/components/Header.tsx
+++ apps/frontend/src/components/Header.tsx
@@ -1,48 +1,34 @@
 import { FaCheckCircle } from "react-icons/fa";
 
-export default function Header({
-  customTitle,
-  showSAAccounting,
-  updatedDesc,
-  showContactButton = true,
-  removeBackgroundColor = false,
-}: {
-  customTitle?: string;
-  showSAAccounting?: boolean;
-  updatedDesc?: string;
-  showContactButton?: boolean;
-  removeBackgroundColor?: boolean;
-}) {
+export default function Header() {
+  // remove all props
   return (
-    <div className={`${removeBackgroundColor ? "" : "bg-green-100"} p-15 lg:flex`}>
+    <div className="bg-green-100 p-15 lg:flex">
       <div className="w-full lg:w-1/2 lg:mx-8">
         <h3 className="text-lg font-semibold text-green-500">
           PROFESSIONAL. RELIABLE. TRUSTED.
         </h3>
-        <h1 className="text-6xl font-bold leading-14">
-          {customTitle ?? `SA Accounting & Tax Solutions You Can Trust`}
+        <h1 className="text-6xl font-bold leading-14 text-green-900">
+          SA Accounting & Tax Solutions You Can Trust
         </h1>
-        <p className="text-lg mt-4">
+        <p className="text-lg mt-4 text-green-900">
           <span className="text-2xl font-bold text-green-700">
-            {(showSAAccounting ?? true) ? "SA Accounting & " : ""}Qureshi's Tax
-            Accounting Services
+            SA Accounting & Qureshi's Tax Accounting Services
           </span>{" "}
-          {updatedDesc ??
-            "are sister concerns, and we are here to help you with personalized and reliable services for you, your business, family, and friends."}
+          "are sister concerns, and we are here to help you with personalized
+          and reliable services for you, your business, family, and friends.
         </p>
-        {showContactButton && (
-          <button
-            className="bg-green-500 text-white hover:cursor-pointer px-4 py-2 rounded mt-4 hover:bg-green-600 transition-colors duration-300"
-            onClick={() => {
-              const connectSection = document.getElementById("connect");
-              if (connectSection) {
-                connectSection.scrollIntoView({ behavior: "smooth" });
-              }
-            }}
-          >
-            Get in Touch
-          </button>
-        )}
+        <button
+          className="bg-green-500 text-white hover:cursor-pointer px-4 py-2 rounded mt-4 hover:bg-green-600 transition-colors duration-300"
+          onClick={() => {
+            const connectSection = document.getElementById("connect");
+            if (connectSection) {
+              connectSection.scrollIntoView({ behavior: "smooth" });
+            }
+          }}
+        >
+          Get in Touch
+        </button>
       </div>
       <div className="flex-1 mx-8 text-lg lg:my-0 my-10">
         <h3 className="font-semibold text-green-500">
